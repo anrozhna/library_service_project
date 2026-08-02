e01965e86f488dc3816c5d27e7890dfc058e40e2
Anna <annalepiska@gmail.com>
2026-08-02 20:03:54 +0300

    feat: add custom User model with email as username field

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "books",
+    "users",
     "rest_framework",
 ]
 
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+AUTH_USER_MODEL = "users.User"

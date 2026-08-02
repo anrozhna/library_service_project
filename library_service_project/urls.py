8a59f90ae7e5caa4d1b5aba205cdaaf309520073
Anna <annalepiska@gmail.com>
2026-08-02 19:24:24 +0300

    feat: register book routes in urls.py

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from books.views import BookViewSet
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    # path("admin/", admin.site.urls),
+    path("", include("books.urls", namespace="books")),
 ]

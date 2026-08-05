8762f4a2490b2e68d6bfaf4d23cca4084d4c3f8f
Anna <annalepiska@gmail.com>
2026-08-05 17:32:42 +0300

    feat: add JWT authentication for BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
 from books.permissions import IsAdminOrReadOnly
@@ -8,4 +9,5 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAdminOrReadOnly,)

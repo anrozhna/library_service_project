e41c9da44c5618630c2e5c83fcf5d737d630046c
Anna <annalepiska@gmail.com>
2026-08-05 18:43:15 +0300

    feat: add borrowing list and detail views

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all().select_related("book", "user")
+    serializer_class = BorrowingSerializer
+    authentication_classes = (JWTAuthentication,)
+
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+        if self.action == "list":
+            serializer = BorrowingListSerializer
+        elif self.action == "retrieve":
+            serializer = BorrowingDetailSerializer
+        return serializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.request.user.is_staff:
+            return queryset
+
+        return queryset.filter(user=self.request.user)

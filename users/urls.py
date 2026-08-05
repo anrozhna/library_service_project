b41a9eeb078a0154fbfff4066be01f3a16f12df3
Anna <annalepiska@gmail.com>
2026-08-05 14:51:01 +0300

    feat: add user profile retrieve and update endpoint (users/me)

--- users/urls.py
+++ users/urls.py
@@ -4,13 +4,17 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import CreateUserView
+from users.views import (
+    CreateUserView,
+    ManageUserView,
+)
 
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "users"

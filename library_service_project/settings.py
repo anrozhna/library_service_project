f701d1dfa923b81128d64cd89f605b1b7d496723
Anna <annalepiska@gmail.com>
2026-08-05 15:34:27 +0300

    chore: rename Authorization header to Authorize for JWT

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -81,6 +81,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=100),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
 }
 
 

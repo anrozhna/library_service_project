2a42896f10fb28b1632a2ad4c285f71c8c466544
Anna <annalepiska@gmail.com>
2026-08-05 20:27:51 +0300

    feat: validate book inventory before creating borrowing

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -15,20 +15,29 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
             "book",
-            "user"
+            "user",
         )
+
 
 class BorrowingListSerializer(BorrowingSerializer):
     book = BookSerializer(read_only=True)
     user = serializers.SlugRelatedField(read_only=True, slug_field="email")
 
+
 class BorrowingDetailSerializer(BorrowingListSerializer):
     user = UserSerializer(read_only=True)
+
 
 class BorrowingCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ("book", "expected_return_date")
+
+    @staticmethod
+    def validate_book(self, value):
+        if value.inventory <= 0:
+            raise serializers.ValidationError("This book is out of stock.")
+        return value
 
 
 class AdminBorrowingCreateSerializer(BorrowingCreateSerializer):

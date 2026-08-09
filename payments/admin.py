a2c1deefdcefdde66751bbee6a2a12ba5ce7e472
Anna <annalepiska@gmail.com>
2026-08-09 18:39:20 +0300

    feat: register Payment model in admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "type", "status", "borrowing", "money_to_pay")
+    list_filter = ("status", "type")

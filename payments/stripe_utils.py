11a3b1b8aca737f13ad480d1f1e9a523b26ed429
Anna <annalepiska@gmail.com>
2026-08-12 13:08:53 +0300

    create Payment with session_url and session_id

--- payments/stripe_utils.py
+++ payments/stripe_utils.py
@@ -1,6 +1,8 @@
 import stripe
 from django.conf import settings
 
+from payments.models import Payment
+
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
@@ -10,6 +12,7 @@ def calculate_borrowing_total_price(borrowing):
     days_to_pay = max(1, days_to_pay)
 
     return days_to_pay * borrowing.book.daily_fee
+
 
 def create_stripe_session(borrowing, success_url, cancel_url):
     """Create a Stripe Checkout Session for a given borrowing."""
@@ -34,4 +37,14 @@ def create_stripe_session(borrowing, success_url, cancel_url):
         success_url=success_url,
         cancel_url=cancel_url,
     )
-    return session
+
+    payment = Payment.objects.create(
+        status=Payment.StatusChoices.PENDING,
+        type=Payment.TypeChoices.PAYMENT,
+        borrowing=borrowing,
+        session_url=session.url,
+        session_id=session.id,
+        money_to_pay=total_price,
+    )
+
+    return payment
